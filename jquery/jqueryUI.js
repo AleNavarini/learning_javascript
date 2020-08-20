919f8d4cf946be4ce7f278b5c677372caf75af59
Alejandro Navarini <anavarini@frro.utn.edu.ar>
2020-08-20 19:35:01 -0300

    advances

--- jquery/jqueryUI.js
+++ jquery/jqueryUI.js
@@ -2,5 +2,35 @@
 $(document).ready(function () {
     $(".element").draggable();
 
-    $(".element").resizable();
+    //$(".element").resizable();
+
+    //$(".selectable-list").selectable();
+    // You can use either selectable or sortable
+    $(".selectable-list").sortable({
+        update: function(event, ui){
+            console.log("there has been a change");
+        }
+    });
+    // drop
+    
+    $('.drop-area').droppable({
+        drop: function(){
+           alert("Entered");
+        }
+    });
+
+    $("#button-effects").click(function(e){
+        $(".drop-area").toggle("slide"); // insert effect you want, explode, drop , etc
+
+    })
+
+
+    //tooltip
+    $(document).tooltip(); // You must include a title in your html elements for this to work
+
+    $("#button-effects").dialog();
+    $("#calendar").datepicker();
+
+    $("#tabs").tabs();
+
 });
