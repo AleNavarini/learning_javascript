34bc96b446fefb005cc86e53682a54441b6a26dd
Alejandro Navarini <anavarini@frro.utn.edu.ar>
2020-08-27 15:40:08 -0300

    starting with angular

--- typescript/ts/car.js
+++ typescript/ts/car.js
@@ -1,4 +1,31 @@
 'use strict';
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+//decorator
+function stampi(logo) {
+    return function (target) {
+        target.prototype.stampation = function () {
+            console.log("Car is stamped with" + logo);
+        };
+    };
+}
 var Car = /** @class */ (function () {
     // Constructor
     function Car(color, model, brand, price) {
@@ -32,6 +59,22 @@ var Car = /** @class */ (function () {
     Car.prototype.setPrice = function (new_price) {
         this.price = new_price;
     };
+    Car = __decorate([
+        stampi('Lambo')
+    ], Car);
     return Car;
 }());
+var Truck = /** @class */ (function (_super) {
+    __extends(Truck, _super);
+    function Truck() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Truck.prototype.getheight = function () {
+        return this.height;
+    };
+    Truck.prototype.setheight = function (new_height) {
+        this.height = new_height;
+    };
+    return Truck;
+}(Car));
 var my_car = new Car("Blue", "Sentra", "Nissan", 10000);
--- typescript/ts/car.ts
+++ typescript/ts/car.ts
@@ -5,6 +5,18 @@ interface CarBase{
     getColor();
     setColor(new_color); // defines the functions that your class must have
 }
+
+
+//decorator
+
+function stampi(logo: string){
+    return function(target: Function){
+        target.prototype.stampation = function():void{
+            console.log("Car is stamped with" + logo);
+        }
+    }
+}
+@stampi('Lambo')
 class Car implements CarBase{ // The name of the class must be the same as the file
     
     // properties
@@ -15,31 +27,31 @@ class Car implements CarBase{ // The name of the class must be the same as the f
 
 
     //Gets and sets
-    public getColor(){
+    public getColor():string{
         return this.color;
     }
-    public setColor(new_color){
+    public setColor(new_color:string){
         this.color = new_color;
     }
     
-    public getModel(){
+    public getModel():string{
         return this.model;
     }
-    public setModel(new_model){
+    public setModel(new_model:string){
         this.model = new_model;
     }
     
-    public getBrand(){
+    public getBrand():string{
         return this.brand;
     }
-    public setBrand(new_brand){
+    public setBrand(new_brand:string){
         this.brand = new_brand;
     }
     
-    public getPrice(){
+    public getPrice():number{
         return this.price;
     }
-    public setPrice(new_price){
+    public setPrice(new_price:number){
         this.price = new_price;
     }
     
